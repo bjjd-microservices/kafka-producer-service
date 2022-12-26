cd447050881b025e85acd1842de8d71d17b8c329
rajivbansal2981 <rajivbansal2981@gmail.com>
2022-12-26 15:16:41 +0530

    JaiMataKi

--- src/main/java/com/jmk/kafka/KafkaProducerServiceApplication.java
+++ src/main/java/com/jmk/kafka/KafkaProducerServiceApplication.java
@@ -1,18 +1,12 @@
 package com.jmk.kafka;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonAutoConfiguration;
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableAsync;
-
-import com.jmk.kafka.ribbon.config.RibbonConfiguration;
-
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableKafka
@@ -20,8 +14,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableSwagger2
-@AutoConfigureAfter(RibbonAutoConfiguration.class)
-@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 @EnableFeignClients(basePackages = "com.jmk")
 @ComponentScan(basePackages = { "com.jmk" })
 public class KafkaProducerServiceApplication {
